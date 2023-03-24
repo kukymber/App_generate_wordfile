6b28dd64cb13880fbc2f4ebbaf6aedb5ed9db60f
kukymber <ostap_to@mail.ru>
2023-03-24 23:31:23 +0800

    Did the main part of app. No image reading and function that choice docxtamplate

--- half_of_app.py
+++ half_of_app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, Canvas, messagebox
+from tkinter import filedialog
+
 import cv2
 import pytesseract
 import re
@@ -35,28 +37,35 @@ words_list = [word for word in filter(
 date_of_birth = re.findall("\d{2}[./,]?\d{2}[./,]?\d{4}", text)
 
 
+def open_image(self):
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        image = cv2.imread(file_path)
+        App(image, file_path)
+
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.dob_entry = None
+        self.name_combos = None
+        self.entry_number = None
+        self.passport_number = None
         self.title("Tkinter App")
         self.geometry("800x800")
         # Creating the date of birth field
         self.date_of_birth_field()
-        self.date_birth = None
 
         # Creating list-based fields for names
         self.name_fields()
-        self.selected_names = ["", "", ""]  # create a list to store the selected names
 
         # Creating the "Who Gave Passport" field with multiple selections
         self.who_gave_passport_field()
-        self.selected_who_gave = []
+        self.selected_who_gave = ["", "", ""]
 
         # Creating the fields for passport series and number
         self.passport_series_number_fields()
         self.selected_seria = []
         self.passport_number_fields()
-        self.passport_number = None
 
         # Adding the canvas for the image
         self.canvas = tk.Canvas(self, width=400, height=600)
@@ -65,25 +74,26 @@ class App(tk.Tk):
         self.show_image()
         self.generate_word_file_button()
 
+
+
     def name_fields(self):
+        self.name_combos = []
         for i in range(3):
             label = ttk.Label(self, text=f"{['First', 'Middle', 'Last'][i]} Name:")
             label.grid(column=0, row=i, padx=10, pady=10)
 
-            self.combo = ttk.Combobox(self, values=words_list[::-1], state="normal")
-            self.combo.grid(column=1, row=i, padx=10, pady=10)
+            combo = ttk.Combobox(self, values=words_list[::-1], state="normal")
+            combo.grid(column=1, row=i, padx=10, pady=10)
+            self.name_combos.append(combo)
 
-        # Bind the on_select_name function to the ComboboxSelected event of the combobox
-        self.combo.bind("<<ComboboxSelected>>", lambda event, index=i: self.on_select_name(event, index))
+            # Bind the on_select_name function to the ComboboxSelected event of the combobox
+            combo.bind("<<ComboboxSelected>>", lambda event, index=i: self.on_select_name(event, index))
 
     def on_select_name(self, event, index):
+        pass
         # Get the selected item from the combobox
-        selected_item = self.combo.get()
-        print(selected_item)
-
-        # Store the selected item in the list of selected names
-        self.selected_names[index] = selected_item
-
+        # selected_item = self.name_combos[index].get()
+        # print(selected_item)
 
     def who_gave_passport_field(self):
         label = ttk.Label(self, text="Who Gave Passport:")
@@ -125,12 +135,10 @@ class App(tk.Tk):
         label = ttk.Label(self, text="Date of Birth:")
         label.grid(column=0, row=5, padx=10, pady=10)
 
-        dob_entry = ttk.Entry(self, state='ACTIVE')
-        dob_entry.insert(0, string=date_of_birth[0])
-        dob_entry.grid(column=1, row=5, padx=10, pady=10)
-        def selected_date_of_birth():
-            self.date_birth = dob_entry.get()
-            print(self.date_birth)
+        self.dob_entry = ttk.Entry(self, state='ACTIVE')
+        self.dob_entry.insert(0, string=date_of_birth[0])
+        self.dob_entry.grid(column=1, row=5, padx=10, pady=10)
+
 
     def passport_series_number_fields(self):
         label_series = ttk.Label(self, text="Passport Series:")
@@ -171,8 +179,9 @@ class App(tk.Tk):
         label_number = ttk.Label(self, text="Passport Number:")
         label_number.grid(column=0, row=8, padx=10, pady=10)
 
-        entry_number = ttk.Combobox(self, values=num_list, state="normal")
-        entry_number.grid(column=1, row=8, padx=10, pady=10)
+        self.entry_number = ttk.Combobox(self, values=num_list, state="normal")
+        self.entry_number.grid(column=1, row=8, padx=10, pady=10)
+
 
     def show_image(self):
         # Load the image using PIL library
@@ -190,35 +199,33 @@ class App(tk.Tk):
         button = ttk.Button(self, text='Generate Word File', command=self.generate_word_file)
         button.grid(column=1, row=9, columnspan=2)
 
-        button.bind("<<ButtonSelected>>")
-
     def generate_word_file(self):
+        selected_items = [combo.get() for combo in self.name_combos]
         # Read the template file
-
         template = DocxTemplate('/home/anatolii/python_project/pythonProject9/template.docx')
 
         # Replace the placeholders with the chosen data
         context = {
-             'first_name': self.name_fields[0].widget.get(),
-             'middle_name': self.name_fields[1].widget.get(),
-             'last_name': self.name_fields[2].widget.get(),
+            'first_name': selected_items[0],
+            'middle_name': selected_items[1],
+            'last_name': selected_items[2],
             'who_gave_passport': ' '.join(self.selected_who_gave),
-            'date_of_birth': self.date_of_birth.get(),
+            'date_of_birth': self.dob_entry.get(),
             'passport_series': ' '.join(self.selected_seria),
-            'passport_number': self.passport_number_fields.get()
+            'passport_number': self.entry_number.get()
         }
 
 
-    # Render the template with the context
+        # Render the template with the context
         template.render(context)
 
         # Save the Word file
-        template.save('output.docx')
+        template.save('outpsut.docx')
 
         # Show a message box to inform the user that the file has been generated
         messagebox.showinfo("File Generated", "The Word file has been generated successfully.")
 
 
 if __name__ == "__main__":
-    app = App()
+    app = open_image()
     app.mainloop()
